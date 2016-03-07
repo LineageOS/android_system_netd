2338babddfcf5f90c574b7a0d470f48dea8001ee
Danesh M <daneshm90@gmail.com>
2016-03-07 15:08:15 -0800

    NetD : Allow passing in interface names for wifi/data app restriction
    CYAN-3976
    CRACKLING-834
    
    Changes from original cm-13.0 patch:
    
    *) Move wifi/cell data app restrict rules into their own chain
       (instead of adding directly to INPUT/OUTPUT).  Now they
       are flushed during soft restart.
    
    *) Introduce helper function manipulateRestrictAppsInOut() to
       reduce code duplication and cleanup the asprintf() /
       manipulateRestrictApps() call sequence.
    
    Change-Id: Ie91f7f4a826047cd6bd0de9a4246285d90ed4c83
    (cherry picked from commit 38e79bccd6091ea0b929183cc9d9167f41eb4a2c)

--- server/CommandListener.cpp
+++ server/CommandListener.cpp
@@ -1281,38 +1281,38 @@ int CommandListener::BandwidthControlCmd::runCommand(SocketClient *cli, int argc
     }
 
     if (!strcmp(argv[1], "addrestrictappsondata")) {
-        if (argc < 3) {
-            sendGenericSyntaxError(cli, "addrestrictappsondata <appUid> ...");
+        if (argc < 4) {
+            sendGenericSyntaxError(cli, "addrestrictappsondata <interface> <appUid> ...");
             return 0;
         }
-        int rc = gCtls->bandwidthCtrl.addRestrictAppsOnData(argc - 2, argv + 2);
+        int rc = gCtls->bandwidthCtrl.addRestrictAppsOnData(argv[2], argc - 3, argv + 3);
         sendGenericOkFail(cli, rc);
         return 0;
     }
     if (!strcmp(argv[1], "removerestrictappsondata")) {
-        if (argc < 3) {
-            sendGenericSyntaxError(cli, "removerestrictappsondata <appUid> ...");
+        if (argc < 4) {
+            sendGenericSyntaxError(cli, "removerestrictappsondata <interface> <appUid> ...");
             return 0;
         }
-        int rc = gCtls->bandwidthCtrl.removeRestrictAppsOnData(argc - 2, argv + 2);
+        int rc = gCtls->bandwidthCtrl.removeRestrictAppsOnData(argv[2], argc - 3, argv + 3);
         sendGenericOkFail(cli, rc);
         return 0;
     }
     if (!strcmp(argv[1], "addrestrictappsonwlan")) {
-        if (argc < 3) {
-            sendGenericSyntaxError(cli, "addrestrictappsonwlan <appUid> ...");
+        if (argc < 4) {
+            sendGenericSyntaxError(cli, "addrestrictappsonwlan <interface> <appUid> ...");
             return 0;
         }
-        int rc = gCtls->bandwidthCtrl.addRestrictAppsOnWlan(argc - 2, argv + 2);
+        int rc = gCtls->bandwidthCtrl.addRestrictAppsOnWlan(argv[2], argc - 3, argv + 3);
         sendGenericOkFail(cli, rc);
         return 0;
     }
     if (!strcmp(argv[1], "removerestrictappsonwlan")) {
-        if (argc < 3) {
-            sendGenericSyntaxError(cli, "removerestrictappsonwlan <appUid> ...");
+        if (argc < 4) {
+            sendGenericSyntaxError(cli, "removerestrictappsonwlan <inteface> <appUid> ...");
             return 0;
         }
-        int rc = gCtls->bandwidthCtrl.removeRestrictAppsOnWlan(argc - 2, argv + 2);
+        int rc = gCtls->bandwidthCtrl.removeRestrictAppsOnWlan(argv[2], argc - 3, argv + 3);
         sendGenericOkFail(cli, rc);
         return 0;
     }

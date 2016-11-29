f66fcced470e6d21c5518c0efd78e7c941db1387
leskal <alexakis1997@gmail.com>
2016-11-29 20:29:45 +0200

    Mediatek devices use ap0 instead of wlan0
    This fixes wifi tether, originally this used the MTK_HARDWARE flag
    but source built kernel mtk's can fix it properly
    
    Change-Id: I4b2c936736b01f159136a008b2dd0db106533ed1

--- server/SoftapController.cpp
+++ server/SoftapController.cpp
@@ -54,7 +54,11 @@ using android::base::WriteStringToFile;
 #endif
 
 #ifdef LIBWPA_CLIENT_EXISTS
+#ifndef MTK_WIFI_HARDWARE
 std:: string hostapd_unix_file(StringPrintf("/data/misc/wifi/hostapd/wlan0"));
+#else
+std:: string hostapd_unix_file(StringPrintf("/data/misc/wifi/hostapd/ap0"));
+#endif
 static const char HOSTAPD_DHCP_DIR[]    = "/data/misc/dhcp";
 #endif
 static const char HOSTAPD_CONF_FILE[]    = "/data/misc/wifi/hostapd.conf";
@@ -289,7 +293,12 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
             "hw_mode=%c\n"
             "ignore_broadcast_ssid=%d\n"
             "wowlan_triggers=any\n",
-            argv[2], argv[3], channel, (channel <= 14) ? 'g' : 'a', hidden));
+#ifndef MTK_WIFI_HARDWARE
+            argv[2]
+#else
+            "ap0"
+#endif
+                , argv[3], channel, (channel <= 14) ? 'g' : 'a', hidden));
 
     std::string fbuf;
     if (argc > 7) {

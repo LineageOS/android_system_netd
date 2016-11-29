e67d28c4bbcb307782dcf311b1f16e82b5c09e52
leskal <alexakis1997@gmail.com>
2016-11-29 20:29:45 +0200

    Mediatek devices use ap0 instead of wlan0
    With this change we enable the wifi tether using the MTK_HARDWARE flag from commit https://review.cyanogenmod.org/#/c/111801/
    
    Change-Id: Ia93a06515837b690f8256abe1ed3a4578f712592

--- server/SoftapController.cpp
+++ server/SoftapController.cpp
@@ -54,7 +54,11 @@ using android::base::WriteStringToFile;
 #endif
 
 #ifdef LIBWPA_CLIENT_EXISTS
+#ifndef MTK_HARDWARE
 static const char HOSTAPD_UNIX_FILE[]    = "/data/misc/wifi/hostapd/wlan0";
+#else
+static const char HOSTAPD_UNIX_FILE[]    = "/data/misc/wifi/hostapd/ap0";
+#endif
 static const char HOSTAPD_SOCKETS_DIR[]    = "/data/misc/wifi/sockets";
 static const char HOSTAPD_DHCP_DIR[]    = "/data/misc/dhcp";
 #endif
@@ -277,7 +281,12 @@ int SoftapController::setSoftap(int argc, char *argv[]) {
             "hw_mode=%c\n"
             "ignore_broadcast_ssid=%d\n"
             "wowlan_triggers=any\n",
-            argv[2], argv[3], channel, (channel <= 14) ? 'g' : 'a', hidden));
+#ifndef MTK_HARDWARE
+            argv[2]
+#else         
+            "ap0"
+#endif
+                , argv[3], channel, (channel <= 14) ? 'g' : 'a', hidden));
 
     std::string fbuf;
     if (argc > 7) {

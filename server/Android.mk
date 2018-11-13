f4b0a250bda144f0bea94acbe53f0567cb414dd4
Kevin F. Haggerty <haggertk@lineageos.org>
2018-11-12 20:23:43 -0700

    netd: Allow devices to opt-out of the tethering active FTP helper
    * AOSP change 4cef4147bfc1c8708c32972191690e1cd449e2e1 enabled a
      conntrack helper for active FTP connections with tethering. This
      does not play well with legacy nf_conntrack kernel implementations.
    * Allow affected devices to opt-out so that tethering actually starts
    
    Change-Id: Ic063142492eb20725e5aab1bbaebac82d74f4211

--- server/Android.mk
+++ server/Android.mk
@@ -68,6 +68,10 @@ ifneq ($(TARGET_PRODUCT), gce_x86_phone)
 endif
 endif
 
+ifeq ($(strip $(TARGET_OMIT_NETD_TETHER_FTP_HELPER)),true)
+        LOCAL_CPPFLAGS += -DOMIT_NETD_TETHER_FTP_HELPER
+endif
+
 LOCAL_INIT_RC := netd.rc
 
 LOCAL_SHARED_LIBRARIES := \

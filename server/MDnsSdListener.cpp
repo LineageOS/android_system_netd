af314f68701a4d6c06ac1b9a09feddcff5e7eb73
Sehee Park <sehee32.park@samsung.com>
2018-12-26 07:28:23 +0900

    Fix fortify_fatal issue during DNSServiceProcessResult()
    fd was checked at beginnig of DNSServiceProcessResult()
    but fd was changed to -1. So, fortify_fatal was occured
    when FD_SET() was called.
    Abort message: 'FORTIFY: FD_SET: file descriptor -1 < 0'
    
    Test: Build
    Bug: 120910016
    Bug: 121327565
    Change-Id: Ib4c8dcc08223578fb53647637b44a20a4c221050
    Merged-In: Ib4c8dcc08223578fb53647637b44a20a4c221050
    Signed-off-by: Sehee Park <sehee32.park@samsung.com>
    (cherry picked from commit 3eeb0e6b86ac8a7f00968d0a086381e7dcd8cc2b)

--- server/MDnsSdListener.cpp
+++ server/MDnsSdListener.cpp
@@ -146,7 +146,7 @@ void MDnsSdListener::Handler::stop(SocketClient *cli, int argc, char **argv, con
         return;
     }
     if (VDBG) ALOGD("Stopping %s with ref %p", str, ref);
-    DNSServiceRefDeallocate(*ref);
+    mMonitor->deallocateServiceRef(ref);
     mMonitor->freeServiceRef(requestId);
     char *msg;
     asprintf(&msg, "%s stopped", str);
@@ -617,7 +617,9 @@ void MDnsSdListener::Monitor::run() {
                         ALOGD("Monitor found [%d].revents = %d - calling ProcessResults",
                                 i, mPollFds[i].revents);
                     }
+                    pthread_mutex_lock(&mHeadMutex);
                     DNSServiceProcessResult(*(mPollRefs[i]));
+                    pthread_mutex_unlock(&mHeadMutex);
                     mPollFds[i].revents = 0;
                 }
             }
@@ -767,5 +769,11 @@ void MDnsSdListener::Monitor::freeServiceRef(int id) {
         }
         prevPtr = &(cur->mNext);
     }
+    pthread_mutex_unlock(&mHeadMutex);
+}
+
+void MDnsSdListener::Monitor::deallocateServiceRef(DNSServiceRef* ref) {
+    pthread_mutex_lock(&mHeadMutex);
+    DNSServiceRefDeallocate(*ref);
     pthread_mutex_unlock(&mHeadMutex);
 }

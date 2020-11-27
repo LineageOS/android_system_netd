CalyxOS/platform_system_netd

system/netd: Add support for Restricted Networking mode
* Add a new firewall chain fw_restricted that blocks all network access.

* Legacy ipchains and bpf are both supported.

Change-Id: Iab892d8d3d1803fe2626b2a5966e5646cb8b4922

bpf_progs/netd.c
@@ -206,6 +206,9 @@ static inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid, int direc
         if ((enabledRules & POWERSAVE_MATCH) && !(uidRules & POWERSAVE_MATCH)) {
             return BPF_DROP;
         }
+        if ((enabledRules & ISOLATED_MATCH) && (uidRules & ISOLATED_MATCH)) {
+            return BPF_DROP;
+        }
     }
     if (direction == BPF_INGRESS && (uidRules & IIF_MATCH)) {
         // Drops packets not coming from lo nor the whitelisted interface

server/TrafficController.cpp
@@ -102,6 +102,7 @@ const std::string uidMatchTypeToString(uint8_t match) {
     FLAG_MSG_TRANS(matchType, POWERSAVE_MATCH, match);
     FLAG_MSG_TRANS(matchType, IIF_MATCH, match);
     FLAG_MSG_TRANS(matchType, IF_BLACKLIST, match);
+    FLAG_MSG_TRANS(matchType, ISOLATED_MATCH, match);
     if (match) {
         return StringPrintf("Unknown match: %u", match);
     }
@@ -670,6 +671,9 @@ int TrafficController::changeUidOwnerRule(ChildChain chain, uid_t uid, FirewallR
         case POWERSAVE:
             res = updateOwnerMapEntry(POWERSAVE_MATCH, uid, rule, type);
             break;
+        case RESTRICTED:
+            res = updateOwnerMapEntry(ISOLATED_MATCH, uid, rule, type);
+            break;
         case NONE:
         default:
             return -EINVAL;
@@ -794,6 +798,8 @@ int TrafficController::replaceUidOwnerMap(const std::string& name, bool isWhitel
         res = replaceRulesInMap(STANDBY_MATCH, uids);
     } else if (!name.compare(FirewallController::LOCAL_POWERSAVE)) {
         res = replaceRulesInMap(POWERSAVE_MATCH, uids);
+    } else if (!name.compare(FirewallController::LOCAL_RESTRICTED)) {
+        res = replaceRulesInMap(ISOLATED_MATCH, uids);
     } else {
         ALOGE("unknown chain name: %s", name.c_str());
         return -EINVAL;
@@ -826,6 +832,9 @@ int TrafficController::toggleUidOwnerMap(ChildChain chain, bool enable) {
             break;
         case POWERSAVE:
             match = POWERSAVE_MATCH;
+            break;
+        case RESTRICTED:
+            match = ISOLATED_MATCH;
             break;
         default:
             return -EINVAL;

LineageOS/android_system_netd

bpf_progs/netd.c
@@ -206,6 +206,9 @@ static inline int bpf_owner_match(struct __sk_buff* skb, uint32_t uid, int direc
         if ((enabledRules & POWERSAVE_MATCH) && !(uidRules & POWERSAVE_MATCH)) {
             return BPF_DROP;
         }
+        if ((enabledRules & RESTRICTED_MATCH) && (uidRules & RESTRICTED_MATCH)) {
+            return BPF_DROP;
+        }
     }
     if (direction == BPF_INGRESS && (uidRules & IIF_MATCH)) {
         // Drops packets not coming from lo nor the whitelisted interface

server/TrafficController.cpp
@@ -102,6 +102,7 @@ const std::string uidMatchTypeToString(uint8_t match) {
     FLAG_MSG_TRANS(matchType, POWERSAVE_MATCH, match);
     FLAG_MSG_TRANS(matchType, IIF_MATCH, match);
     FLAG_MSG_TRANS(matchType, IF_BLACKLIST, match);
+    FLAG_MSG_TRANS(matchType, RESTRICTED_MATCH, match);
     if (match) {
         return StringPrintf("Unknown match: %u", match);
     }
@@ -670,6 +671,9 @@ int TrafficController::changeUidOwnerRule(ChildChain chain, uid_t uid, FirewallR
         case POWERSAVE:
             res = updateOwnerMapEntry(POWERSAVE_MATCH, uid, rule, type);
             break;
+        case RESTRICTED:
+            res = updateOwnerMapEntry(RESTRICTED_MATCH, uid, rule, type);
+            break;
         case NONE:
         default:
             return -EINVAL;
@@ -794,6 +798,8 @@ int TrafficController::replaceUidOwnerMap(const std::string& name, bool isWhitel
         res = replaceRulesInMap(STANDBY_MATCH, uids);
     } else if (!name.compare(FirewallController::LOCAL_POWERSAVE)) {
         res = replaceRulesInMap(POWERSAVE_MATCH, uids);
+    } else if (!name.compare(FirewallController::LOCAL_RESTRICTED)) {
+        res = replaceRulesInMap(RESTRICTED_MATCH, uids);
     } else {
         ALOGE("unknown chain name: %s", name.c_str());
         return -EINVAL;
@@ -826,6 +832,9 @@ int TrafficController::toggleUidOwnerMap(ChildChain chain, bool enable) {
             break;
         case POWERSAVE:
             match = POWERSAVE_MATCH;
+            break;
+        case RESTRICTED:
+            match = RESTRICTED_MATCH;
             break;
         default:
             return -EINVAL;